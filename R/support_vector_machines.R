993d6c9b813b9a729ce67f27015e327aecdc850d
David Solá <david@adsmurai.com>
2017-06-02 13:55:14 +0200

    Delete support vector machines script because it is never been used anymore

--- R/support_vector_machines.R
+++
@@ -1,6 +0,0 @@
-# https://www.svm-tutorial.com/2014/10/support-vector-regression-r/
-library(e1071)
-appearsDF <- as.data.frame(appearsProcessed)
-
-## model 1: LINEAR kernel, C=1 (cost parameter)
-(model <- svm(appearsDF[,-1],appearsDF[,1], type="C-classification", cost=1, kernel="linear", scale = FALSE))
